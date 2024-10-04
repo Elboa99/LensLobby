d307c8e9f02b8fa5d15ed31b0144a824f7384940
Kenny Boateng <kennyboateng.99@gmail.com>
2024-10-04 18:36:42 +0200

    Initial commit

---
+++ src/main/java/kennyboateng/LensLobby/LensLobbyApplication.java
@@ -0,0 +1,13 @@
+package kennyboateng.LensLobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensLobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensLobbyApplication.class, args);
+	}
+
+}
